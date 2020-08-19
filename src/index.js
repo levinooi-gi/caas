bd5d6e6bbb3f9815dd258fd4fe0181c332a155f3
levinooi-gi <justin.levinooi@gmail.com>
2020-08-19 15:30:50 +0000

    change to head

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
-import "./js/jquery-3.5.1.min.js";
-import "./js/floating-wpp.min.js";
+// import "./js/jquery-3.5.1.min.js";
+// import "./js/floating-wpp.min.js";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
