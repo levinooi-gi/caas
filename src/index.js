ce0be193c1e6ecc6dfebc2d1e837c2247edf6c8f
levinooi-gi <justin.levinooi@gmail.com>
2020-08-19 17:56:35 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import "./css/main.css";
 // import "./js/jquery-3.5.1.min.js";
 // import "./js/floating-wpp.min.js";
 // Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+// console.log("🦊 Hello! Edit me in src/index.js");
