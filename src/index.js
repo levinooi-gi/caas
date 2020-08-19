e0ba5fec1e98932b7e57224fddc08e2fbcc43923
levinooi-gi <justin.levinooi@gmail.com>
2020-08-19 15:22:27 +0000

    js fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
-
+import "./js/floating-wpp.min.js";
+import "./js/jquery-3.5.1.min.js";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
-import "./js/floating.wpp.min.js";
-import "./js/jquery-3.5.1.min.js";
